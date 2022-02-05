e6be31858835abe24621148e1df29db12dc13405
Alex Beregszaszi <alex@rtfs.hu>
2022-02-05 02:29:00 +0100

    Update interface to newest EIP

--- contracts/protocols/eip4626/IEIP4626.sol
+++ contracts/protocols/eip4626/IEIP4626.sol
@@ -5,35 +5,47 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 interface IEIP4626 is IERC20, IERC20Metadata {
-    function deposit(address to, uint256 value) external returns (uint256 shares);
+    event Deposit(address indexed sender, address indexed receiver, uint256 assets, uint256 shares);
 
-    function mint(address to, uint256 shares) external returns (uint256 value);
+    event Withdraw(address indexed sender, address indexed receiver, uint256 assets, uint256 shares);
+
+    function asset() external view returns (address assetTokenAddress);
+
+    function totalAssets() external view returns (uint256 totalAssetsControlled);
+
+    function assetsPerShare() external view returns (uint256 assetsPerUnitShare);
+
+    function assetsOf(address owner) external view returns (uint256 assets);
+
+    function maxDeposit(address caller) external view returns (uint256 maxAssets);
+
+    function previewDeposit(uint256 assets) external view returns (uint256 shares);
+
+    function deposit(uint256 assets, address receiver) external returns (uint256 shares);
+
+    function maxMint(address caller) external view returns (uint256 maxShares);
+
+    function previewMint(uint256 shares) external view returns (uint256 assets);
+
+    function mint(uint256 shares, address receiver) external returns (uint256 value);
+
+    function maxWithdraw(address caller) external view returns (uint256 maxAssets);
+
+    function previewWithdraw(uint256 assets) external view returns (uint256 shares);
 
     function withdraw(
-        address from,
-        address to,
-        uint256 value
+        uint256 assets,
+        address receiver,
+        address owner
     ) external returns (uint256 shares);
 
+    function maxRedeem(address caller) external view returns (uint256 maxShares);
+
+    function previewRedeem(uint256 shares) external view returns (uint256 assets);
+
     function redeem(
-        address from,
-        address to,
-        uint256 shares
-    ) external returns (uint256 value);
-
-    function underlying() external view returns (address);
-
-    function totalUnderlying() external view returns (uint256);
-
-    function balanceOfUnderlying(address owner) external view returns (uint256);
-
-    function exchangeRate() external view returns (uint256);
-
-    function previewDeposit(uint256 underlyingAmount) external view returns (uint256 shareAmount);
-
-    function previewMint(uint256 shareAmount) external view returns (uint256 underlyingAmount);
-
-    function previewWithdraw(uint256 underlyingAmount) external view returns (uint256 shareAmount);
-
-    function previewRedeem(uint256 shareAmount) external view returns (uint256 underlyingAmount);
+        uint256 shares,
+        address receiver,
+        address owner
+    ) external returns (uint256 assets);
 }
